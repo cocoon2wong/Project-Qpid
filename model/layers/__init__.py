6bddb776e9ff11700043b031ac89d6cd0c6c990e
conghaowong <conghaowong@icloud.com>
2023-09-06 21:13:21 +0800

    move old `silverballers`-related layers to `qpid.model.layers`

--- model/layers/__init__.py
+++ model/layers/__init__.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2021-12-21 15:22:27
 @LastEditors: Conghao Wong
-@LastEditTime: 2023-06-15 16:26:41
+@LastEditTime: 2023-09-06 19:47:38
 @Description: file content
 @Github: https://github.com/cocoon2wong
 @Copyright 2022 Conghao Wong, All Rights Reserved.
@@ -12,6 +12,7 @@ from . import interpolation, transfroms
 from .__flatten import Flatten, Padding
 from .__graphConv import GraphConv
 from .__linear import LinearLayer, LinearLayerND
+from .__outer import OuterLayer
 from .__pooling import MaxPooling2D
 from .__traj import ContextEncoding, TrajEncoding
 from .transfroms import get_transform_layers
--- silverballers/__init__.py
+++ silverballers/__init__.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2022-06-20 22:09:51
 @LastEditors: Conghao Wong
-@LastEditTime: 2023-09-06 19:07:31
+@LastEditTime: 2023-09-06 19:54:45
 @Description: file content
 @Github: https://github.com/cocoon2wong
 @Copyright 2022 Conghao Wong, All Rights Reserved.
@@ -14,3 +14,5 @@ from .__baseArgs import SilverballersArgs
 from .__handlerModel import BaseHandlerModel, BaseHandlerStructure, HandlerArgs
 from .__MKII_model import SilverballersMKII, SilverballersModel
 from .__MKII_utils import SILVERBALLERS_DICT
+
+register = SILVERBALLERS_DICT.register
